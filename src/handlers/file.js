1624c4f27e2972b71564aef89a81493e85a7e8d3
Jack Wan <hwguyguy@gmail.com>
2018-03-04 21:06:43 +0800

    Update file handler

--- src/handlers/file.js
+++ src/handlers/file.js
@@ -2,15 +2,14 @@ const {existsSync, createReadStream} = require('fs')
 const {lookup} = require('mime-types')
 const {root} = require('../helpers/path')
 
-exports.serve = async function ({response, url}) {
-	const filePath = root(url.pathname.substr(1))
+exports.serve = async function (context) {
+	const filePath = root(context.url.pathname.substr(1))
 	if (existsSync(filePath)) {
 		const contentType = lookup(filePath) || 'application/octet-stream'
-		response.setHeader('Content-Type', contentType)
-		response.writeHead(200)
-		createReadStream(filePath).pipe(response)
+		const readStream = createReadStream(filePath)
+		context.response.setHeader('Content-Type', contentType)
+		context.respond(readStream)
 	} else {
-		response.writeHead(404)
-		response.end('File not found')
+		context.respond(null, 404)
 	}
 }
