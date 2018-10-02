dca013461e392df54c3ba738269aad49e21f31e5
Jack Wan <hwguyguy@gmail.com>
2018-10-02 22:08:32 +0800

    Update webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const ENV = process.env.NODE_ENV || 'development'
 const ROOT_PATH = path.resolve(__dirname, '..')
@@ -10,10 +11,9 @@ function root(...args) {
 	return path.join.apply(path, args)
 }
 
-const mainCss = new ExtractTextPlugin('main.css')
-const adminCss = new ExtractTextPlugin('admin.css')
-
 module.exports = {
+	mode: ENV,
+
 	entry: {
 		main: [
 			root('assets/scripts/main/index.js'),
@@ -29,7 +29,6 @@ module.exports = {
 		path: root('dist'),
 		filename: '[name].js',
 		sourceMapFilename: '[file].map',
-		chunkFilename: '[id].chunk.js',
 	},
 
 	resolve: {
@@ -41,94 +40,17 @@ module.exports = {
 	//    'ckeditor': 'CKEDITOR',
 	//},
 
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					babelrc: false,
-					presets: [
-						['env', {
-							targets: Object.assign(
-								{
-									browsers: ['last 2 versions'],
-								},
-								(ENV === 'production' && {
-									uglify: true,
-								})
-							),
-						}],
-					],
-					plugins: [
-						'transform-class-properties',
-						'transform-object-rest-spread',
-					],
-				},
-			},
-
-			{
-				include: root('assets/styles/main/index.styl'),
-				use: mainCss.extract({
-					fallback: 'style-loader',
-					use: [{
-						loader: 'css-loader',
-						options: {
-							minimize: ENV === 'production',
-						}
-					}, {
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: true,
-							plugins: () => [
-								require('autoprefixer')({
-									browsers: ['> 1%', 'not ie <= 8'],
-									remove: false,
-								}),
-							]
-						}
-					}, {
-						loader: 'stylus-loader',
-						options: {
-							sourceMap: true,
-						}
-					}],
-				})
-			},
-
-			{
-				include: root('assets/styles/admin/index.styl'),
-				use: adminCss.extract({
-					fallback: 'style-loader',
-					use: [{
-						loader: 'css-loader',
-						options: {
-							minimize: ENV === 'production',
-						}
-					}, {
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: true,
-							plugins: () => [
-								require('autoprefixer')({
-									browsers: ['> 1%', 'not ie <= 8'],
-									remove: false,
-								}),
-							]
-						}
-					}, {
-						loader: 'stylus-loader',
-						options: {
-							sourceMap: true,
-						}
-					}],
-				})
-			},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin(),
 		]
 	},
 
 	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+
 		...(ENV === 'development' ? [
 			new webpack.LoaderOptionsPlugin({
 				debug: true
@@ -140,24 +62,72 @@ module.exports = {
 			}),
 		]: []),
 
-		mainCss,
-		adminCss,
+		new webpack.DefinePlugin({
+			'process.env': {
+				ENV: JSON.stringify(ENV),
+				NODE_ENV: JSON.stringify(ENV),
+			}
+		}),
+	],
 
-		...(ENV === 'production' ? [
-			new webpack.DefinePlugin({
-				'process.env': {
-					'ENV': JSON.stringify(ENV),
-					'NODE_ENV': JSON.stringify(ENV),
-				}
-			}),
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					babelrc: false,
+					presets: [
+						['@babel/preset-env', {
+							targets: Object.assign(
+								{
+									browsers: ['last 2 versions'],
+								},
+								(ENV === 'production' && {
+									uglify: true,
+								})
+							),
+						}],
+					],
+					plugins: [
+						require('@babel/proposal-class-properties'),
+						require('@babel/proposal-object-rest-spread'),
+					],
+				},
+			},
 
-			new webpack.optimize.UglifyJsPlugin({
-				compress: true,
-				mangle: true,
-				beautify: false,
-				comments: false,
-			}),
-		]: []),
-
-	]
+			{
+				test: /\.styl/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+							minimize: ENV === 'production',
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+							plugins: () => [
+								require('autoprefixer')({
+									browsers: ['> 1%', 'not ie <= 8'],
+									remove: false,
+								}),
+							]
+						}
+					},
+					{
+						loader: 'stylus-loader',
+						options: {
+							sourceMap: true,
+						}
+					}
+				]
+			},
+		]
+	},
 }
